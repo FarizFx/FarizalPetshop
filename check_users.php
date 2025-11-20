fa7872e23431c04d516375f8fb2c8da1dd31c0e6
Mad Farizi <madfarizi@gmail.com>
2025-11-21 05:24:02 +0700

    delete

--- check_users.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "./function/connection.php";
-
-$query = mysqli_query($connection, "SELECT * FROM user LIMIT 5");
-while($row = mysqli_fetch_assoc($query)) {
-    echo $row['nama'] . ' - ' . $row['role'] . PHP_EOL;
-}
-?>
--- update_permissions.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'function/connection.php';
-
-$query = "UPDATE roles SET permissions = JSON_SET(permissions, '$.purchase_management', true) WHERE role_name IN ('super_admin', 'admin', 'manager', 'staff')";
-$result = mysqli_query($connection, $query);
-echo $result ? 'Permissions updated successfully' : 'Error: ' . mysqli_error($connection);
-?>
