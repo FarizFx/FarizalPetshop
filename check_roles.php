8bfc304ccb0b81fcabaa596661bee6b321e70afb
Mad Farizi <86289386+FarizFx@users.noreply.github.com>
2025-11-21 05:21:56 +0700

    Delete

--- check_roles.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "./function/connection.php";
-
-$query = mysqli_query($connection, "SELECT * FROM roles");
-while($row = mysqli_fetch_assoc($query)) {
-    echo $row['role_name'] . ': ' . $row['permissions'] . PHP_EOL;
-}
-?>
